21744f3399b6be86feeb30ff77bf2dcd5b5d2769
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-28 14:08:46 -0700

    agregamos de poder actualizar desde el servidor los datos para que queden guardados

--- Components/Data/ServicioJuegos.cs
+++ Components/Data/ServicioJuegos.cs
@@ -9,13 +9,37 @@ namespace primerproyecto.Components.Data
             new Juego { Identificador = 1, Nombre = "Fortnite", Jugado = true },
             new Juego { Identificador = 2, Nombre = "GTA 6", Jugado = false },
         };
+
         public Task<List<Juego>> ObtenerJuegos() => Task.FromResult(juegos);
-                    
+
+        public Task<List<Juego>> ActualizarDatosDesdeServidor()
+        {
+            return Task.FromResult(new List<Juego>(juegos));
+        }
+
         public Task AgregarJuego(Juego juego)
         {
             juegos.Add(juego);
             return Task.CompletedTask;
         }
+
+        public Task CambiarEstadoJuego(int identificador)
+        {
+            var juego = juegos.FirstOrDefault(j => j.Identificador == identificador);
+            if (juego != null)
+            {
+                juego.Jugado = !juego.Jugado;
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task CambiarEstadoTodos(bool nuevoEstado)
+        {
+            foreach (var juego in juegos)
+            {
+                juego.Jugado = nuevoEstado;
+            }
+            return Task.CompletedTask;
+        }
     }
 }
-    
--- Components/Servicio/ServicioControlador.cs
+++ Components/Servicio/ServicioControlador.cs
@@ -5,26 +5,43 @@ namespace primerproyecto.Components.Servicio
     public class ServicioControlador
     {
         private readonly ServicioJuegos _servicioJuegos;
-        
+
         public ServicioControlador(ServicioJuegos servicioJuegos)
-            
-            {
+
+        {
             _servicioJuegos = servicioJuegos;
         }
-    public async Task<List<Juego>> ObtenerJuegos()
-    {
-        return await _servicioJuegos.ObtenerJuegos();
-        } 
+
+        public async Task<List<Juego>> ObtenerJuegos()
+        {
+            return await _servicioJuegos.ObtenerJuegos();
+        }
+
+        public async Task<List<Juego>> ActualizarDatosDesdeServidor()
+        {
+            return await _servicioJuegos.ActualizarDatosDesdeServidor();
+        }
+
         public async Task AgregarJuego(Juego juego)
         {
             juego.Identificador = await GenerarNuevoID();
-            _servicioJuegos.AgregarJuego(juego);
+            await _servicioJuegos.AgregarJuego(juego);
         }
-        private async Task <int> GenerarNuevoID()
+
+        public async Task CambiarEstadoJuego(int identificador)
+        {
+            await _servicioJuegos.CambiarEstadoJuego(identificador);
+        }
+
+        public async Task CambiarEstadoTodos(bool nuevoEstado)
+        {
+            await _servicioJuegos.CambiarEstadoTodos(nuevoEstado);
+        }
+
+        private async Task<int> GenerarNuevoID()
         {
             var juegos = await _servicioJuegos.ObtenerJuegos();
             return juegos.Any() ? juegos.Max(j => j.Identificador) + 1 : 1;
         }
-
     }
 }
